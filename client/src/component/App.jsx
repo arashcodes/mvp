b5ff2d790b0bf3b9f33afce12f6c63eff993ff7c
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-01-11 14:38:37 -0800

    Set up app structure

---
+++ client/src/component/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>
+        Hello from App.jsx
+      </div>
+    )
+  }
+}
+
+export default App;
